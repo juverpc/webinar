5c1162ce0daec6a052d96725ec3d73a8276c26a8
juverpc <juverpc@gmail.com>
2025-09-06 16:40:50 +0000

    testing 11

--- modules/ec2/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip" {
-  description = "La dirección IP pública de la instancia EC2."
-  value       = aws_instance.web_server.public_ip
-}
--- modules/network/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr" {
-  description = "El bloque CIDR para la VPC."
-  type        = string
-}
-
-variable "public_subnet_cidr" {
-  description = "El bloque CIDR para la subred pública."
-  type        = string
-}
