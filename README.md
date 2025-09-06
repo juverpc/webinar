e7fb8dd560ded8091bdc7ed5343c51abc4f7e21e
juverpc <juverpc@gmail.com>
2025-09-06 15:48:51 +0000

    testing 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # webinar
 demo webinar
+
--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "terraform-bucket-juver" 
-    key            = "terraform.tfstate"  
+    key            = "environment/test/terraform.tfstate"  
     region         = "us-east-1"  
     encrypt        = true  # Habilita la encriptación del estado
     dynamodb_table = "terraform-locks"  # Tabla DynamoDB para el bloqueo de estado
